5933c9b7d7063937cfa55d1497492cd761bddb9f
Adam Vogiatzis <ad.vog@protonmail.com>
2021-05-22 21:59:26 +0300

    BuildFormForAddAGig

---
+++ ArtHub/Controllers/GigController.cs
@@ -0,0 +1,30 @@
+﻿using ArtHub.Models;
+using ArtHub.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtHub.Controllers
+{
+    public class GigController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public GigController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Gig
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres=context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
