2cbbb88bcf3fbdff8f582a1edc4c499262d1ab35
Adam Vogiatzis <ad.vog@protonmail.com>
2021-06-02 21:07:07 +0300

    AddAttendancesApiController

---
+++ ArtHub/Controllers/api/AttendancesController.cs
@@ -0,0 +1,37 @@
+﻿using ArtHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ArtHub.Controllers.api
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext context;
+
+        public AttendancesController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(int gigId)
+        {
+            var attendance = new Attendance
+            {
+                GigId=gigId,
+                AttendeeId=User.Identity.GetUserId()
+            };
+
+            context.Attendances.Add(attendance);
+            context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- ArtHub/Global.asax.cs
+++ ArtHub/Global.asax.cs
@@ -2,9 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
 using System.Web.Mvc;
 using System.Web.Optimization;
-using System.Web.Routing;
 
 namespace ArtHub
 {
@@ -12,6 +13,7 @@ namespace ArtHub
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
