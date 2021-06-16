ef6b2f81d0d4145ff676a151e715e5b417538902
Adam Vogiatzis <ad.vog@protonmail.com>
2021-06-16 21:31:05 +0300

    NotificationsFrontEnd

--- ArtHub/App_Start/BundleConfig.cs
+++ ArtHub/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace ArtHub
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/underscore.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,7 +25,8 @@ namespace ArtHub
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/animate.css"));
         }
     }
 }
